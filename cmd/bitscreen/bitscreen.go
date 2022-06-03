aefea2cdf4910317fa9cc7acf746c56e1593bf4e
Stefan Manciu <manciustefan@gmail.com>
2022-06-03 12:22:38 +0300

    Checking multiple CIDs from same deal & new unit tests

--- cmd/bitscreen/bitscreen.go
+++ cmd/bitscreen/bitscreen.go
@@ -7,10 +7,12 @@ import (
 	"github.com/Jeffail/gabs"
 	"github.com/Murmuration-Labs/bitscreen"
 	"github.com/ipfs/go-cid"
+	"fmt"
 )
 
-func getDealInfo() (cid.Cid, error) {
-	var c cid.Cid
+func getDealInfo() ([]cid.Cid, error) {
+	var cids []cid.Cid
+
 	f, err := os.OpenFile("/tmp/bitscreen_go.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if err != nil {
         log.Fatalf("error opening file: %v", err)
@@ -26,30 +28,41 @@ func getDealInfo() (cid.Cid, error) {
 
 	for _, path := range [][]string{
 		[]string{"Ref", "Root", "/"}, // storage deal
+		[]string{"Proposal", "PieceCID", "/"}, // storage deal
 		[]string{"PayloadCID", "/"}, // retrieval deal
 	} {
 		c, err := cid.Parse(proposal.Search(path...).Data())
 		// check only if found a valid CID
 		if err == nil {
-			return c, err
+			cids = append(cids, c);
 		}
 	}
-  return c, errors.New("No valid CID found.")
+
+	var cidErr error;
+
+    if len(cids) == 0 {
+        cidErr = errors.New("No valid CID found.")
+    }
+
+    return cids, cidErr
 }
 
 func main() {
-    cid, err := getDealInfo()
+    cids, err := getDealInfo()
     if err != nil {
         os.Exit(1)
     }
 
-    if bitscreen.IsLoadFromFileEnabled() {
-        if bitscreen.BlockCidFromFile(cid) {
-            os.Exit(1)
-        }
-    } else {
-        if bitscreen.BlockCidFromProcess(cid) {
-            os.Exit(1)
+    for _, cid := range cids {
+        fmt.Println("Checking "+cid.String())
+        if bitscreen.IsLoadFromFileEnabled() {
+            if bitscreen.BlockCidFromFile(cid) {
+                os.Exit(1)
+            }
+        } else {
+            if bitscreen.BlockCidFromProcess(cid) {
+                os.Exit(1)
+            }
         }
     }
 }
